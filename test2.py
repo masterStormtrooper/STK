eae3e5cd73e9a9f23f9fe33dedef93ef7bb4fcf2
luoying5 <yingchengluo@gmail.com>
2018-11-21 17:45:54 -0500

    ws ape

--- test2.py
+++ test2.py
@@ -1,6 +1,4 @@
-"""Test 2"""
-import MA
-import Reader
+from STK import MA, Reader
 import time
 import matplotlib.pyplot as plt
 start_time = time.time()
@@ -11,9 +9,9 @@ MAs = (5, 10)
 
 
 data = Reader.readfile('td.csv')
-new = MA.profit_MA(data, MAs, date_range)
+new=MA.profit_MA(data, MAs, date_range)
 # print(new)
-new.plot_price(date_range, True)
+new.plot_price(date_range, True, True)
 plt.show()
 
 print("--- %s seconds ---" % (time.time() - start_time))
